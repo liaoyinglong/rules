d714d2ecd64149e71cbca69b967bfd28d21a26e3
liaoyinglong <vigossliao@gmail.com>
2024-12-01 16:12:03 +0800

    wip

---
+++ src/upsteam-config.ts
@@ -0,0 +1,14 @@
+export const upstreamConfig = [
+  {
+    name: 'twitter',
+    url: 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Twitter/Twitter.yaml',
+  },
+  {
+    name: 'google',
+    url: 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Google/Google.yaml',
+  },
+  {
+    name: 'telegram',
+    url: 'https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.yaml',
+  },
+];
